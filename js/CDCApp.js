534c0605761d40f81cff936b6c78bfd2c210758b
Diego Plascencia <diego-d@outlook.com>
2015-06-27 01:34:16 -0500

    modify template routes

--- js/CDCApp.js
+++ js/CDCApp.js
@@ -3,19 +3,19 @@ CDCApp.config(['$routeProvider',
            function($routeProvide){
                $routeProvide.
                    when('/', {
-                       templateUrl: '../Views/main.html',
+                       templateUrl: './Views/main.html',
                        controller: 'mainController'
                    })
                    .when('/enlace', {
-                       templateUrl: '../Views/enlace.html',
+                       templateUrl: './Views/enlace.html',
                        controller: 'enlaceController'
                    })
                    .when('/inmuebles', {
-                       templateUrl: '../Views/inmuebles.html',
+                       templateUrl: './Views/inmuebles.html',
                        controller: 'inmueblesController'
                    })
                    .when('/cemabe', {
-                       templateUrl: '../Views/cemabe.html',
+                       templateUrl: './Views/cemabe.html',
                        controller: 'cemabeController'
                    })
                    .otherwise({
